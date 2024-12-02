19d3891d5e25afb936d1e43bce8fc73c74b4a9ad
sedra.ranaivoson <sedraranaivoson@gmail.com>
2024-12-02 14:48:51 +0300

    Mise en place de l'interface pour cocktails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Cocktail } from './interfaces/cocktail.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'myapp';
+  cocktail: Cocktail = {
+    name: 'Mojito',
+    img: 'https://images.ricardocuisine.com/services/wysiwyg/3346.jpg',
+    description:
+      'Originaire de Cuba, le mojito est une boisson populaire à base de rhum mélangé avec de la menthe, du jus de lime et du sucre de canne. L’été ne serait pas ce qu’il serait sans un grand verre de ce cocktail pour accompagner nos 5 à 7!',
+  };
 }
