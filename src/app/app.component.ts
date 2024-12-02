bbab7b2936ffc139b88a57e9eb4658da4fcd5b47
sedra.ranaivoson <sedraranaivoson@gmail.com>
2024-12-02 15:36:31 +0300

    Ajout détails d'un cocktail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,4 @@ import { Cocktail } from './interfaces/cocktail.interface';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  // cocktail: Cocktail = {
-  //   name: 'Mojito',
-  //   img: 'https://images.ricardocuisine.com/services/wysiwyg/3346.jpg',
-  //   description:
-  //     'Originaire de Cuba, le mojito est une boisson populaire à base de rhum mélangé avec de la menthe, du jus de lime et du sucre de canne. L’été ne serait pas ce qu’il serait sans un grand verre de ce cocktail pour accompagner nos 5 à 7!',
-  // };
-}
+export class AppComponent {}
